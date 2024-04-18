bd968aa43bab5410206d9c9e1cbed2e5f9d56749
Anthony Ryan <anthonyryan1@gmail.com>
2024-04-18 11:33:38 -0400

    Require maxminddb extension for faster geoip lookups

--- lookup.php
+++ lookup.php
@@ -1,12 +1,18 @@
 <?php
+
+declare(strict_types=1);
+
 header('Content-type: application/json');
 
 if (is_file(__DIR__ . "/vendor/autoload.php")) {
-        // Dependencies installed via composer
-        require(__DIR__ . "/vendor/autoload.php");
+    // Dependencies installed via composer
+    require(__DIR__ . "/vendor/autoload.php");
 } elseif (is_file('/usr/share/php/GeoIP2/autoload.php')) {
-        // Dependencies installed via Fedora autoloader
-        require('/usr/share/php/GeoIP2/autoload.php');
+    // Dependencies installed via Fedora autoloader
+    require('/usr/share/php/GeoIP2/autoload.php');
+} else {
+    http_response_code(501);
+    exit;
 }
 
 use GeoIp2\Database\Reader AS GeoIP2;
@@ -16,13 +22,13 @@ $Return = [];
 $GeoIP2 = new GeoIP2('/usr/share/GeoIP/GeoLite2-Country.mmdb');
 
 if (isset($_POST['ip']) && is_array($_POST['ip'])) {
-	foreach ($_POST['ip'] as $IP) {
-		try {
-			$Return[] = ['ip' => $IP, 'info' => ['country' => strtolower($GeoIP2->country($IP)->country->isoCode), 'host' => $IP]];
-		} catch (Exception $e) {
-			$Return[] = ['ip' => $IP, 'info' => ['country' => 'un', 'host' => $IP]];
-		}
-	}
+    foreach ($_POST['ip'] as $IP) {
+        try {
+            $Return[] = ['ip' => $IP, 'info' => ['country' => strtolower($GeoIP2->country($IP)->country->isoCode), 'host' => $IP]];
+        } catch (Exception $e) {
+            $Return[] = ['ip' => $IP, 'info' => ['country' => 'un', 'host' => $IP]];
+        }
+    }
 }
 
 echo json_encode($Return);
